f5da453a576ec84dd6b1f160466bdbbff1980a07
G Mack <mack.gerrit@gmail.com>
2019-08-13 16:09:30 -0700

    app-2 complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,19 +3,22 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(){
+    super();
+    this.state = {
+      arr: ['one', 'two', 'three']
+    };
   }
+  
+  render() {
+    let numsToDisplay = this.state.arr.map((element, index) => {
+      return <h2 key={index}>{element}</h2>
+    });
+
+    return <div className='App'>{numsToDisplay}</div>;
+  }
+
 }
+ 
 
 export default App;
